ca063f6b13525e86c8851fc0f75f2f8ebe77a087
Pshemcio <daglas92@gmail.com>
2020-12-18 17:20:09 +0100

    Add stop, reset buttons

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ window.addEventListener('DOMContentLoaded', () => {
     usernameInput.addEventListener('keyup', addUser);
     timeInput.addEventListener('keyup', addTime);
     players.addEventListener('click', removeUser);
-    spinBtn.addEventListener('click', twisterSpin);
+    controls.addEventListener('click', handleBtnClick);
+
 });
 
 const usernameInput = document.getElementById('username-input');
 const timeInput = document.getElementById('time-input');
 const usernameDisplay = document.getElementById('username-display');
 const infoDisplay = document.getElementById('info-display');
-const spinBtn = document.getElementById('spin');
-const players = document.querySelector('.players');
+const players = document.querySelector('.players-list');
 const timer = document.getElementById('timer');
 const errorMsg = document.getElementById('error-msg');
 const timeStamp = document.querySelector('.time-stamp');
+const units = document.getElementById('units');
+const controls = document.querySelector('.controls');
 
 const dataObj = {
     players: [],
@@ -22,6 +24,8 @@ const dataObj = {
     bodyParts: ['lewa ręka', 'prawa ręka', 'lewa noga', 'prawa noga'],
     time: 0
 };
+
+let $timerInterval;
 
 const onlyNumbers = input => {
     input.target.value = input.target.value.replace(/[E\+\-]/gi, "");
@@ -54,6 +58,7 @@ const addTime = input => {
     if (input.key === 'Enter') {
         dataObj.time = parseInt(input.target.value);
         timer.textContent = dataObj.time;
+        input.target.value < 5 ? units.textContent = ' sekundy' : units.textContent = ' sekund';
         input.target.value = '';
         errorMsg.textContent = '';
         timeStamp.classList.add('active');
@@ -107,7 +112,21 @@ const removeUser = user => {
     errorMsg.textContent = '';
 };
 
-const twisterSpin = button => {
+const handleBtnClick = button => {
+    switch (button.target.id) {
+        case 'start':
+            handleStart(button);
+            break;
+        case 'stop':
+            handleStop(button);
+            break;
+        case 'reset':
+            handleReset(button);
+            break;
+    };
+};
+
+const handleStart = button => {
     if (dataObj.players.length === 0) {
         errorMsg.textContent = 'Dodaj graczy!';
         return;
@@ -120,13 +139,28 @@ const twisterSpin = button => {
         return;
     };
 
-    button.target.classList.add('active');
+    controls.classList.add('active');
 
     const spinInterval = dataObj.time * 1000;
 
     let playerIndex = 0;
 
-    setInterval(() => {
+    if (infoDisplay.textContent === '') {
+        const countDown = (secs) => {
+            var timer = setInterval(() => {
+                if (secs < 1) {
+                    clearInterval(timer);
+                    return infoDisplay.textContent = 'Start!';
+                };
+                infoDisplay.textContent = secs;
+                secs--;
+            }, 1000);
+        };
+
+        countDown(dataObj.time - 2);
+    }
+
+    $timerInterval = window.setInterval(() => {
         let randomNum1 = Math.floor((Math.random() * 4));
         let randomNum2 = Math.floor((Math.random() * 4));
 
@@ -138,3 +172,84 @@ const twisterSpin = button => {
         infoDisplay.textContent = `${dataObj.bodyParts[randomNum1]} na ${dataObj.colors[randomNum2]}`;
     }, spinInterval);
 };
+
+const handleStop = () => {
+    controls.classList.remove('active');
+    clearInterval($timerInterval);
+};
+
+const handleReset = () => {
+    dataObj.players = [];
+    dataObj.time = 0;
+    const playersList = players.querySelectorAll('li');
+    const allParagraphs = document.getElementsByClassName('js-reset');
+
+    for (let i = 0; i < allParagraphs.length; i++) {
+        const paragraph = allParagraphs[i];
+
+        paragraph.textContent = '';
+    };
+
+    playersList.forEach(player => {
+        player.remove();
+    });
+
+    clearInterval($timerInterval);
+};
+
+
+
+
+// const handleSpeech = () => {
+//     var synth = window.speechSynthesis;
+
+//     var inputForm = document.querySelector('form');
+//     var inputTxt = document.querySelector('.txt');
+//     var voiceSelect = document.querySelector('select');
+
+//     var pitch = document.querySelector('#pitch');
+//     var pitchValue = document.querySelector('.pitch-value');
+//     var rate = document.querySelector('#rate');
+//     var rateValue = document.querySelector('.rate-value');
+
+//     var voices = [];
+
+//     function populateVoiceList() {
+//         voices = synth.getVoices();
+
+//         for (var i = 0; i < voices.length; i++) {
+//             var option = document.createElement('option');
+//             option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+//             if (voices[i].default) {
+//                 option.textContent += ' -- DEFAULT';
+//             }
+
+//             option.setAttribute('data-lang', voices[i].lang);
+//             option.setAttribute('data-name', voices[i].name);
+//             voiceSelect.appendChild(option);
+//         }
+//     }
+
+//     populateVoiceList();
+//     if (speechSynthesis.onvoiceschanged !== undefined) {
+//         speechSynthesis.onvoiceschanged = populateVoiceList;
+//     }
+
+//     inputForm.onsubmit = function (event) {
+//         event.preventDefault();
+
+//         var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+//         var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+//         for (var i = 0; i < voices.length; i++) {
+//             if (voices[i].name === selectedOption) {
+//                 utterThis.voice = voices[i];
+//             };
+//         };
+//         utterThis.pitch = pitch.value;
+//         utterThis.rate = rate.value;
+//         synth.speak(utterThis);
+
+//         inputTxt.blur();
+//     };
+// };
